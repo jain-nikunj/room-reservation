632db18a15fad48b3c3967b6ff6b879816e8273c
Zhanshi Wang <zhanshi.wang.verdandi@gmail.com>
2017-10-31 22:59:36 +0000

    added homepage filters, fixed map labels, added map navigating boundaries

--- app/controllers/buildings_controller.rb
+++ app/controllers/buildings_controller.rb
@@ -1,7 +1,39 @@
 class BuildingsController < ApplicationController
-  
-  
+
   def index
+    file = File.read(Rails.public_path + "building_geo.json")
+    geo_data = JSON.parse(file)
+    @rooms = Room.all
+    if params[:StudentAccessible]
+      @rooms = @rooms.where("facilities LIKE '%ADA-Student Accessible%'")
+    end
+    if params[:Whiteboard]
+      @rooms = @rooms.where("facilities LIKE '%Board-White%' OR facilities LIKE '%Board-Front%'")
+    end
+    if params[:AV]
+      @rooms = @rooms.where("facilities LIKE '%AV%'")
+    end
+    if params.key?(:room_type)
+      if params[:room_type] != "Any"
+        @rooms = @rooms.where(:misc => params[:room_type])
+      end
+    end
+    building_ids = Set.new
+    @rooms.select(:building_id).group(:building_id).each do |data|
+      building_ids.add(data.building_id)
+    end
+    puts building_ids
+    update_features = []
+    geo_data["features"].each do |building|
+      if building_ids.include? building["id"]
+        update_features.push building
+      end
+    end
+    geo_data["features"] = update_features
+    File.open(Rails.public_path + "updated_building_geo.json","w") do |f|
+      f.write(geo_data.to_json)
+    end 
+
   end
 
   def search
