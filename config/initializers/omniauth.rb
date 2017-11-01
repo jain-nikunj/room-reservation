9a1b0bcd6d312293d1613ed34d5159b5597973e7
Sunny <sunny.singh.13@berkeley.edu>
2017-11-01 03:42:44 +0000

    fixing ssl certs

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   {
       
       hd: 'berkeley.edu',
-      :client_options => {:ssl => {:ca_file => '/usr/lib/ssl/certs/ca-certificates.crt'}}
+      :client_options => {:ssl => {:ca_file => Rails.root.join('lib/assests/cacert.pem').to_s}}
   }
   
 end
