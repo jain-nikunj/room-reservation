afcd6d66750a801e2ac026d5f7d0ad258a854503
Sunny <sunny.singh.13@berkeley.edu>
2017-11-01 03:48:11 +0000

    updated oauth ssl

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -5,7 +5,7 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   {
       
       hd: 'berkeley.edu',
-      :client_options => {:ssl => {:ca_file => Rails.root.join('lib/assests/cacert.pem').to_s}}
+      :client_options => {:ssl => {:verify => false}}
   }
   
 end
