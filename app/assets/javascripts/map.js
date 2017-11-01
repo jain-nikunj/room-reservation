51af9d9065c9d1ea8250d123147f11c7c4a0872e
Mark Shen <markshen818@hotmail.com>
2017-11-01 10:51:07 +0000

    reimplement map to remove hard coded geojson dependency

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,4 +1,5 @@
 var map;
+var markers = [];
 
 function initMap() {
   var mapCenter = {lat: 37.8719402, lng: -122.2622687};
@@ -8,8 +9,6 @@ function initMap() {
     mapTypeControl: false,
     fullscreenControl: false
   });
-  
-  map.data.loadGeoJson("updated_building_geo.json");
   
   // Make sure user won't navigate the map out of the scope of Berkeley.
   var allowedBounds = new google.maps.LatLngBounds(
@@ -27,29 +26,86 @@ function initMap() {
     map.panTo(lastValidCenter);
   });
   
-  map.data.addListener('click', function(event) {
-    var studentAccessible = document.getElementById("StudentAccessible").checked;
-    var whiteboard = document.getElementById("Whiteboard").checked;
-    var AV = document.getElementById("AV").checked;
-    var typeDrop = document.getElementById("type_drop");
-    var roomType = typeDrop.options[typeDrop.selectedIndex].value;
-    var capacityDrop = document.getElementById("capacity_drop");
-    var capacity = capacityDrop.options[capacityDrop.selectedIndex].value;
-    var paramsString = "?utf8=✓";
-    paramsString += studentAccessible ? "&StudentAccessible=true" : "";
-    paramsString += whiteboard ? "&Whiteboard=true" : "";
-    paramsString += AV ? "&AV=true" : "";
-    paramsString += "&room_type=" + roomType;
-    paramsString += "&capacity=" + capacity;
-    window.location.href = '/buildings/' + event.feature.getId() + paramsString;
-  });
+  updateMarkers();
+}
+
+function filterMarkers(e) {
+  e.preventDefault();
+  updateMarkers();
+}
+
+function updateMarkers() {
+  var studentAccessible = document.getElementById("StudentAccessible").checked;
+  var whiteboard = document.getElementById("Whiteboard").checked;
+  var AV = document.getElementById("AV").checked;
+  var typeDrop = document.getElementById("type_drop");
+  var roomType = typeDrop.options[typeDrop.selectedIndex].value;
+  var capacityDrop = document.getElementById("capacity_drop");
+  var capacity = capacityDrop.options[capacityDrop.selectedIndex].value;
+  var paramsString = "?utf8=✓";
+  paramsString += studentAccessible ? "&StudentAccessible=true" : "";
+  paramsString += whiteboard ? "&Whiteboard=true" : "";
+  paramsString += AV ? "&AV=true" : "";
+  paramsString += "&room_type=" + roomType;
+  paramsString += "&capacity=" + capacity;
   
-  map.data.addListener('mouseover', function(event) {
-    document.getElementById('legend').style.visibility = 'visible';
-    legend.innerHTML = event.feature.getProperty("name");
-  });
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      postMarkers(JSON.parse(this.responseText));
+    }
+  };
+  xhttp.open("GET", "/filter" + paramsString, true);
+  xhttp.send();
+}
+
+function postMarkers(data) {
+  // clear existing markers
+  var length = markers.length;
+  for (var i = 0; i < length; i++) {
+    markers[i].setMap(null);
+  }
+  markers = [];
   
-  map.data.addListener('mouseout', function(event) {
-    document.getElementById('legend').style.visibility = 'hidden';
-  });
+  // for each new building, query for geolocation and create a new marker
+  length = data.length;
+  for (var i = 0; i < length; i++) {
+    addMarker(data[i]['name'], data[i]['id'])
+  }
+}
+
+function addMarker(name, id) {
+  var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+        result = JSON.parse(this.responseText);
+        console.log(name);
+        geo = result['results'][0]['geometry']['location'];
+        var marker = new google.maps.Marker({
+          position: geo,
+          map: map,
+          title: name,
+          id: id,
+        });
+        markers.push(marker);
+        google.maps.event.addListener(marker, 'click', function() {
+          var studentAccessible = document.getElementById("StudentAccessible").checked;
+          var whiteboard = document.getElementById("Whiteboard").checked;
+          var AV = document.getElementById("AV").checked;
+          var typeDrop = document.getElementById("type_drop");
+          var roomType = typeDrop.options[typeDrop.selectedIndex].value;
+          var capacityDrop = document.getElementById("capacity_drop");
+          var capacity = capacityDrop.options[capacityDrop.selectedIndex].value;
+          var paramsString = "?utf8=✓";
+          paramsString += studentAccessible ? "&StudentAccessible=true" : "";
+          paramsString += whiteboard ? "&Whiteboard=true" : "";
+          paramsString += AV ? "&AV=true" : "";
+          paramsString += "&room_type=" + roomType;
+          paramsString += "&capacity=" + capacity;
+          window.location.href = '/buildings/' + marker.id + paramsString;
+        });
+      }
+    };
+    xhttp.open("GET", `https://maps.googleapis.com/maps/api/geocode/json?address=${name}%20Hall`, true);
+    xhttp.send();
 }
