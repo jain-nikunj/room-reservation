54ed982130ac83c3932f234ad2489b3f7e3fd015
Mark Shen <markshen818@hotmail.com>
2017-11-18 04:20:19 +0000

    add Clear button functionality, restore login constraints

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -3,6 +3,22 @@ function details_filter() {
     window.location.href = window.location.href.split('?')[0] + paramsString, true;
 }
 
+function clear_filters() {
+    var roomType = document.getElementById('roomtype_column');
+    var inputs = roomType.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].checked = true;
+    }
+    
+    var features = document.getElementById('facilities_column');
+    inputs = features.getElementsByTagName('input');
+    for (var i = 0; i < inputs.length; i++) {
+        inputs[i].checked = false;
+    }
+    
+    document.getElementById('filters-submit').click();
+}
+
 /*  Limit the value of capacity field to be greater than or equal to 0,
  *  and lower bound <= upper bound.
  */
