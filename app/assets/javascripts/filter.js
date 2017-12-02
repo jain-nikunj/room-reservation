85d721b1839c614f9e56b31bce43234026011c7d
Kathryn Wu <kathryn_wu1997@berkeley.edu>
2017-12-02 08:45:00 +0000

    [test]:qwu.153312663.home page tags done

--- app/assets/javascripts/filter.js
+++ app/assets/javascripts/filter.js
@@ -28,3 +28,30 @@ function limit() {
     capacityLower.value = Math.max(0, capacityLower.value);
 	capacityUpper.value = Math.max(capacityLower.value, capacityUpper.value);
 }
+
+function filter_update(e) {
+    var classroom = document.getElementById("Classroom").checked;
+    var lectureHall = document.getElementById("LectureHall").checked;
+    var auditorium = document.getElementById("Auditorium").checked;
+    var seminarRoom = document.getElementById("SeminarRoom").checked;
+    var studentAccessible = document.getElementById("StudentAccessible").checked;
+    var board = document.getElementById("Board").checked;
+    var AV = document.getElementById("AV").checked;
+    classroom ? $('#Classroom_tag').show() : $('#Classroom_tag').hide();
+    lectureHall ? $('#LectureHall_tag').show() : $('#LectureHall_tag').hide();
+    auditorium ? $('#Auditorium_tag').show() : $('#Auditorium_tag').hide();
+    seminarRoom ? $('#SeminarRoom_tag').show() : $('#SeminarRoom_tag').hide();
+    studentAccessible ? $('#StudentAccessible_tag').show() : $('#StudentAccessible_tag').hide();
+    board ? $('#Board_tag').show() : $('#Board_tag').hide();
+    AV ? $('#AV_tag').show() : $('#AV_tag').hide();
+    
+    filterMarkers(e);
+}
+
+function remove_tag(tag_name) {
+    checkbox = document.getElementById(tag_name);
+    checkbox.checked = false;
+    updateMarkers()
+    tag = document.getElementById(tag_name + "_tag");
+    $(tag).hide()
+}
